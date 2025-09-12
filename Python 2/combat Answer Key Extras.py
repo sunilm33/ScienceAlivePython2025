023d0d92370f0b6bbd66cb96b27c6405ddc1b426
sunilm33 <78884617+sunilm33@users.noreply.github.com>
2025-09-12 11:45:53 -0700

    Done combat answer key

--- Python 2/combat Answer Key Extras.py
+++ Python 2/combat Answer Key Extras.py
@@ -5,6 +5,7 @@ import time
 actions = ["sword", "shield", "bow"]
 win = False
 max_p_health = 15
+p_damage = 5
 # Function to get the winner of a round
 
 def choose_winner(player_choice, enemy_choice):
@@ -30,6 +31,7 @@ def main():
     continue_game = True
     current_p_health = max_p_health
     enemy_health = random.randint(10,20)
+    e_damage = random.randint(1,p_damage)
     win = False
     while continue_game:
         print(f"Player Health = {current_p_health}, Enemy Health = {enemy_health}")
@@ -48,9 +50,9 @@ def main():
         result = choose_winner(player_choice, enemy_choice)
         time.sleep(1)
         if result == -1:
-            current_p_health -= 5
+            current_p_health -= e_damage
         elif result == 1:
-            enemy_health -= 5
+            enemy_health -= p_damage
 
         if (enemy_health <= 0 and current_p_health <= 0) or current_p_health <= 0:
             continue_game = False 
@@ -70,3 +72,12 @@ while True:
     if play_again == "no":
         print("Goodbye! Thank you for playing.")
         break
+    else:
+        while True:
+            choice = input("Would you like to upgrade health or attack? ")
+            if choice.lower() == "health":
+                max_p_health += 2
+                break
+            elif choice.lower() == "attack":
+                p_damage += 2
+                break
